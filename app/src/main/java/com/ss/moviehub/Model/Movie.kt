54db115b3e9af870c672c580d073e045f93db4e8
Abdulrahman-AlGhamdi <xxd7ooom.ssxx@gmail.com>
2020-10-18 15:04:35 +0300

    adding model class for the movies

---
+++ app/src/main/java/com/ss/moviehub/Model/Movie.kt
@@ -0,0 +1,11 @@
+package com.ss.moviehub.Model
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    val release_date: String,
+    val overview: String,
+    val status: String,
+    val runtime: Int,
+    val original_language: String
+)
