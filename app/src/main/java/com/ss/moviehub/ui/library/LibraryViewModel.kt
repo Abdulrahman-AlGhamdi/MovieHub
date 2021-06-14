bd0662f0543e898ee2f01e0ff4f27309c9681f0b
Abdulrahman Al-Ghamdi <49812896+Abdulrahman-AlGhamdi@users.noreply.github.com>
2021-06-14 15:56:10 +0300

    Show empty message in library list

--- app/src/main/java/com/ss/moviehub/ui/library/LibraryViewModel.kt
+++ app/src/main/java/com/ss/moviehub/ui/library/LibraryViewModel.kt
@@ -4,8 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ss.moviehub.models.Result
 import com.ss.moviehub.repository.DatabaseRepository
+import com.ss.moviehub.ui.library.LibraryViewModel.LibraryMoviesState.*
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,7 +23,11 @@ class LibraryViewModel @Inject constructor(
         viewModelScope.launch { databaseRepository.deleteAllMovies() }
     }
 
-    fun getLibraryMovies() = databaseRepository.getLibraryMovies()
+    suspend fun getLibraryMovies() = flow {
+        databaseRepository.getLibraryMovies().collect {
+            if (it.isEmpty()) emit(EmptyList) else emit(LibraryList(it))
+        }
+    }.flowOn(Dispatchers.IO)
 
     fun addMovieToLibrary(result: Result) {
         viewModelScope.launch { databaseRepository.addMovieToLibrary(result) }
@@ -25,5 +35,10 @@ class LibraryViewModel @Inject constructor(
 
     fun deleteMovieFromLibrary(result: Result) {
         viewModelScope.launch { databaseRepository.deleteMovieLibrary(result) }
+    }
+
+    sealed class LibraryMoviesState {
+        object EmptyList : LibraryMoviesState()
+        data class LibraryList(val libraryList: List<Result>) : LibraryMoviesState()
     }
 }
